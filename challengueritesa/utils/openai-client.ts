816a9fcf8165d28ad14dba0d2e631f2ffa1d707a
TixeiraValentin <valen.tixeira@gmail.com>
2024-02-15 03:26:29 -0300

    fix: clear check and completion

--- challengueritesa/utils/openai-client.ts
+++ challengueritesa/utils/openai-client.ts
@@ -1,10 +1,5 @@
 import OpenAI from 'openai';
 
-const apiKey = process.env.OPENAI_SECRET_KEY;
-if (!apiKey) {
-  throw new Error('The OPENAI_API_KEY is not defined');
-}
-
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_SECRET_KEY,
 });
